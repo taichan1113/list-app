af83316f51bd6ab0672de5a98214872385fa79dd
taichan <takecchi.1113@gmail.com>
2020-05-10 19:19:00 +0900

    offline disconnect handling works successfully

--- app.js
+++ app.js
@@ -19,43 +19,56 @@ let db_config_offline = {
   database: 'applist'
 };
 
-let pool = mysql.createPool(db_config_offline);
+let connection;
+
+handleDisconnect = () => {
+  connection = mysql.createConnection(db_config_online);
+  connection.connect((err) => {
+    if (err) {
+      setTimeout(handleDisconnect, 1000);
+    }
+  });
+  connection.on('error', (err) => {
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+      handleDisconnect();
+    } else {
+      throw error;
+    }
+  });
+};
+
+handleDisconnect();
+
 
 dateNull = (date) => date? date : null;
 
 app.get('/', (req, res) => {
-  pool.getConnection((err, connection) => {
-    connection.query(
-      'SELECT * FROM foodstock_taichi ORDER BY expirationDate ASC, expirationType IS NULL ASC, expirationType ASC',
-      (error, results) => {
-        res.render('top.ejs', {items: results, url: req.url});
-      }
-    );
-  });
+  connection.query(
+    'SELECT * FROM foodstock_taichi ORDER BY expirationDate ASC, expirationType IS NULL ASC, expirationType ASC',
+    (error, results) => {
+      res.render('top.ejs', {items: results, url: req.url});
+    }
+  );
 });
 
 app.post('/delete/:id', (req, res) => {
-  pool.getConnection((err, connection) => {
-    connection.query(
-      'delete from foodstock_taichi where id = ?',
-      [req.params.id],
-      (error, results) => {
-        res.redirect('/');
-      }
-    );
-  });
+  connection.query(
+    'delete from foodstock_taichi where id = ?',
+    [req.params.id],
+    (error, results) => {
+      res.redirect('/');
+    }
+  );
 });
 
 app.post('/add', (req, res) => {
-  pool.getConnection((err, connection) => {
-    connection.query(
-      'insert into foodstock_taichi (name, purchaseDate, expirationDate, expirationType) values (?, ?, ?, ?)',
-      [req.body.itemName, dateNull(req.body.itemPurchaseDate), dateNull(req.body.itemExpirationDate), req.body.itemType],
-      (error, results) => {
-        res.redirect('/');
-      }
-    );
-  });
+  connection.query(
+    'insert into foodstock_taichi (name, purchaseDate, expirationDate, expirationType) values (?, ?, ?, ?)',
+    [req.body.itemName, dateNull(req.body.itemPurchaseDate), dateNull(req.body.itemExpirationDate), req.body.itemType],
+    (error, results) => {
+      res.redirect('/');
+    }
+  );
 });
 
 app.listen(app.get('port'));
--- initialDB.sql
+++ initialDB.sql
@@ -1,4 +1,4 @@
 create database Expiration;
 use Expiration;
-create table food (id int auto_increment, name text, purchaseDate date, shohi date, shomi date, primary key (id));
+create table foodstock_taichi (id int auto_increment, name text, purchaseDate date, expirationDate date, expirationType int, primary key (id));
 
